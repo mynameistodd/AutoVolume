00d386caf4633ede742ba11bf6fd1a4ddc75f283
Todd DeLand <todd.deland@gmail.com>
2015-07-21 08:01:21 -0400

    Change to use RecyclerView!

--- Auto Volume/src/main/java/com/mynameistodd/autovolume/AlarmListFragment.java
+++ Auto Volume/src/main/java/com/mynameistodd/autovolume/AlarmListFragment.java
@@ -1,17 +1,13 @@
 package com.mynameistodd.autovolume;
 
 import android.app.Activity;
-import android.app.ListFragment;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.ActionMode;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.ViewGroup;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
@@ -22,13 +18,18 @@ import java.util.List;
 /**
  * Created by todd on 1/11/14.
  */
-public class AlarmListFragment extends ListFragment {
+public class AlarmListFragment extends Fragment implements AlarmRecyclerAdapter.IAdapterClicks {
 
     public static GoogleAnalytics analytics;
     public static Tracker tracker;
-    AlarmListCallbacks mCallbacks;
-    private ArrayAdapter<Alarm> adapter;
+
+    private AlarmListCallbacks mCallbacks;
     private List<Alarm> alarms;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     public AlarmListFragment() {
     }
@@ -39,86 +40,90 @@ public class AlarmListFragment extends ListFragment {
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        alarms = new ArrayList<Alarm>();
-        alarms.addAll(MySQLiteOpenHelper.getAllAlarms(getActivity()));
-        alarms.addAll(CalendarHelper.getAllAlarms(getActivity()));
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alarm_list, container, false);
 
-        adapter = new AlarmAdapter(getActivity(), android.R.layout.list_content, alarms);
-        setListAdapter(adapter);
-        setEmptyText(getActivity().getString(R.string.no_alarms));
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
 
-        ListView listView = getListView();
-        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
-        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
-            List<Alarm> selAlarms = new ArrayList<Alarm>();
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-            @Override
-            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
-                Log.d(Util.MYNAMEISTODD, "Position: " + position);
-                Log.d(Util.MYNAMEISTODD, "ID: " + id);
-
-                Alarm alarm = (Alarm) getListView().getItemAtPosition(position);
-                if (checked) {
-                    if (!selAlarms.contains(alarm)) {
-                        selAlarms.add(alarm);
-                    }
-                } else {
-                    if (selAlarms.contains(alarm)) {
-                        selAlarms.remove(alarm);
-                    }
-                }
-
-                mode.setTitle(selAlarms.size() + " selected");
-            }
-
-            @Override
-            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-                MenuInflater inflater = mode.getMenuInflater();
-                inflater.inflate(R.menu.activity_edit_create_alarm, menu);
-                return true;
-            }
-
-            @Override
-            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-                return false;
-            }
-
-            @Override
-            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.menu_delete:
-                        deleteSelected(selAlarms);
-                        mode.finish();
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-
-            @Override
-            public void onDestroyActionMode(ActionMode mode) {
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-
-    }
-
-    private void deleteSelected(List<Alarm> selAlarms) {
-        for (Alarm alarm : selAlarms) {
-            alarms.remove(alarm);
-            alarm.delete();
-        }
+        return view;
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Alarm item = (Alarm) getListView().getItemAtPosition(position);
-        mCallbacks.onAlarmSelected(item.getId());
+    public void onResume() {
+        super.onResume();
+        alarms = new ArrayList<>();
+        alarms.addAll(MySQLiteOpenHelper.getAllAlarms(getActivity()));
+        alarms.addAll(CalendarHelper.getAllAlarms(getActivity()));
+
+        mAdapter = new AlarmRecyclerAdapter(getActivity(), alarms, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+//        ListView listView = getListView();
+//        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+//        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+//            List<Alarm> selAlarms = new ArrayList<Alarm>();
+//
+//            @Override
+//            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+//                Log.d(Util.MYNAMEISTODD, "Position: " + position);
+//                Log.d(Util.MYNAMEISTODD, "ID: " + id);
+//
+//                Alarm alarm = (Alarm) getListView().getItemAtPosition(position);
+//                if (checked) {
+//                    if (!selAlarms.contains(alarm)) {
+//                        selAlarms.add(alarm);
+//                    }
+//                } else {
+//                    if (selAlarms.contains(alarm)) {
+//                        selAlarms.remove(alarm);
+//                    }
+//                }
+//
+//                mode.setTitle(selAlarms.size() + " selected");
+//            }
+//
+//            @Override
+//            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+//                MenuInflater inflater = mode.getMenuInflater();
+//                inflater.inflate(R.menu.activity_edit_create_alarm, menu);
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.menu_delete:
+//                        deleteSelected(selAlarms);
+//                        mode.finish();
+//                        return true;
+//                    default:
+//                        return false;
+//                }
+//            }
+//
+//            @Override
+//            public void onDestroyActionMode(ActionMode mode) {
+//                adapter.notifyDataSetChanged();
+//            }
+//        });
     }
+
+//    private void deleteSelected(List<Alarm> selAlarms) {
+//        for (Alarm alarm : selAlarms) {
+//            alarms.remove(alarm);
+//            alarm.delete();
+//        }
+//    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -133,8 +138,13 @@ public class AlarmListFragment extends ListFragment {
         tracker = analytics.newTracker(R.xml.global_tracker);
     }
 
+    @Override
+    public void onItemClick(int position) {
+        Alarm item = alarms.get(position);
+        mCallbacks.onAlarmSelected(item.getId());
+    }
+
     public interface AlarmListCallbacks {
         void onAlarmSelected(int id);
     }
 }
-
---
+++ Auto Volume/src/main/java/com/mynameistodd/autovolume/AlarmRecyclerAdapter.java
@@ -0,0 +1,124 @@
+package com.mynameistodd.autovolume;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by todd on 1/11/15.
+ */
+public class AlarmRecyclerAdapter extends RecyclerView.Adapter<AlarmRecyclerAdapter.ViewHolder> {
+
+    Integer mMaxVolume;
+    private List<Alarm> mAlarms;
+    private Context mContext;
+    private IAdapterClicks mListener;
+
+    public AlarmRecyclerAdapter(Context context, List<Alarm> places, IAdapterClicks listener) {
+        this.mContext = context;
+        this.mAlarms = places;
+        this.mListener = listener;
+        this.mMaxVolume = ((AudioManager) context.getSystemService(Context.AUDIO_SERVICE)).getStreamMaxVolume(AudioManager.STREAM_RING);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_list_alarm_item, parent, false);
+        return new ViewHolder(view, new IViewHolderClicks() {
+            @Override
+            public void onItemClick(int position) {
+                mListener.onItemClick(position);
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        final Alarm alarm = mAlarms.get(i);
+
+        //set the time
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, alarm.getHour());
+        c.set(Calendar.MINUTE, alarm.getMinute());
+        viewHolder.mTime.setText(DateUtils.formatDateTime(mContext, c.getTimeInMillis(), DateUtils.FORMAT_SHOW_TIME));
+
+        //set the recur
+        String textToShow = Util.getRecurText(alarm.getRecur());
+        viewHolder.mRecur.setText(textToShow);
+
+        //set the volume
+        viewHolder.mVolume.setText(Util.getVolumePercent(String.valueOf(alarm.getVolume()), mMaxVolume));
+
+        //set the on/off switch
+        viewHolder.mEnabled.setChecked(alarm.isEnabled());
+        viewHolder.mEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    alarm.schedule();
+                } else {
+                    alarm.cancel();
+                }
+                alarm.setEnabled(isChecked);
+                alarm.save();
+            }
+        });
+
+        viewHolder.mTitle.setText(alarm.getTitle());
+
+        if (alarm.getType() == Alarm.AlarmType.Calendar) {
+            viewHolder.itemView.setAlpha(0.3F);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAlarms.size();
+    }
+
+    public interface IViewHolderClicks {
+        void onItemClick(int position);
+    }
+
+    public interface IAdapterClicks {
+        void onItemClick(int position);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public IViewHolderClicks mListener;
+
+        public TextView mTime;
+        public TextView mRecur;
+        public TextView mVolume;
+        public CompoundButton mEnabled;
+        public TextView mTitle;
+
+        public ViewHolder(View itemView, IViewHolderClicks listener) {
+            super(itemView);
+            mListener = listener;
+
+            mTime = (TextView) itemView.findViewById(R.id.tv_time);
+            mRecur = (TextView) itemView.findViewById(R.id.tv_recur);
+            mVolume = (TextView) itemView.findViewById(R.id.tv_volume);
+            mEnabled = (CompoundButton) itemView.findViewById(R.id.switch1);
+            mTitle = (TextView) itemView.findViewById(R.id.tv_title);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mListener.onItemClick(getPosition());
+        }
+    }
+}
